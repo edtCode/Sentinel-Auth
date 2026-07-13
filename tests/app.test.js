13714ae0ab79b700d954687f8bd5cdf7d5b01d81
edtCode <noshivam19@gmail.com>
2026-07-13 18:09:40 +0530

    add app.test

---
+++ tests/app.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest")
+
+const app = require('/src/app')
+
+describe("Application", () => {
+    it("should return 404 for unknown routes", async() => {
+        const response = await request(app).get("/unknown-route");
+
+        expect(response.statusCode).toBe(404)
+        
+    })
+})
