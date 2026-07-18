92c6616a4234013d6018cc6ae283a0257f024cba
edtCode <noshivam19@gmail.com>
2026-07-18 20:18:12 +0530

    make login test more better

--- tests/auth/login.test.js
+++ tests/auth/login.test.js
@@ -4,12 +4,13 @@ const { createUser } = require("../helpers/user")
 
 const app = require('../../src/app')
 const { email } = require('zod')
+const pool = require('../../src/config/db')
 
 describe("Login api", () => {
     describe("/api/auth/login", () => {
         it("should ogin user successfully", async() => {
 
-            await createUser()
+            const user = await createUser()
 
             const response = await request(app)
             .post("/api/auth/login")
@@ -18,7 +19,16 @@ describe("Login api", () => {
                 password: "Password@123"
             })
 
-            expect(app.response.statusCode).toBe(200)
+            expect(response.statusCode).toBe(200)
+            expect(response.body.accessToken).toBeDefined()
+            expect(response.body.refreshToken).toBeDefined()
+
+            const refreshTokenResult = await pool.query(
+                `SELECT * FROM refresh_tokens WHERE user_id = $1`,
+                [user.id]
+            )
+
+            expect(refreshTokenResult.rows.length).toBe(1)
 
         })
 
