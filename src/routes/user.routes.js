9ef283d53093517f6c9ffd8df3908ce416cb142a
edtCode <noshivam19@gmail.com>
2026-08-06 13:13:03 +0530

    restore swagger docs annotations

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,23 @@ const authenticate = require('../middlewares/auth.middleware')
 
 const { getProfile } = require('../controllers/auth.controllers')
 
+/**
+ * @swagger
+ * /api/users/me:
+ *   get:
+ *     summary: Get current user profile
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User profile retrieved successfully
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: User not found
+ */
 router.get("/me",authenticate,getProfile)
 
 module.exports = router
